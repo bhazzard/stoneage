25b191863ad9b6b4f28f44b64fd325b4d55c1005
Michael Dellanoce <michael.dellanoce@gmail.com>
2012-01-02 16:27:17 -0500

    Bootstrapping worker placement

--- src/engine.js
+++ src/engine.js
@@ -33,9 +33,21 @@ define(['underscore', 'src/player'], function(_, Player) {
       if (!this._options.pickPlayers) {
          throw "pickPlayers is a required callback"
       }
-      var players = _(this._options.pickPlayers()).map(function(player) {
+
+      this._players = _(this._options.pickPlayers()).map(function(player) {
         return new Player(player.color);
       });
+
+      if (!this._options.workersChanged) {
+         throw "workersChanged is a required callback"
+      }
+      
+      _(this._players).each(function(player) {
+        this._options.workersChanged(player._color, 'playerBoard', 5);
+      }, this);
+
+      this._options.workersChanged(this._players[0]._color, 'forest', 2);
+      this._options.workersChanged(this._players[0]._color, 'playerBoard', 3);
    };
 
    return Engine;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 require(['src/engine'], function(Engine) {
    $(function() {
-      var stage, board, forest;
+      var stage, board, forest, piles;
       
       stage = $('<div/>')
          .attr('id', 'stage');
@@ -10,24 +10,32 @@ require(['src/engine'], function(Engine) {
          .appendTo(stage);
 
       forest = $('<div/>')
-         .attr('id', 'forest')
+         .addClass('forest')
          .addClass('resourceSpace')
          .appendTo(board);
 
+      $('<div/>').addClass('worker-pile').addClass('player-red').appendTo(forest);
+
       stage.appendTo('body');
 
+      var playerBoard = $('<div />').addClass('playerBoard').appendTo(stage);
+      $('<div/>').addClass('worker-pile').addClass('player-red').appendTo(playerBoard);
+      $('<div/>').addClass('worker-pile').addClass('player-green').appendTo(playerBoard);
+
       var engine = new Engine({
-         placeWorkers: function(player, space, quantity) {
-            $('<div/>')
-               .addClass('worker')
-               .addClass('player-' + player)
-               .addClass('quantity-' + quantity)
-               .html(quantity)
-               .appendTo('#' + space);
+        workersChanged: function(player, space, quantity) {
+            $('.' + space).find('.worker-pile.player-' + player).html(quantity);
          },
          removeWorkers: function(player, space) {
-            $('#' + space + ' .player-' + player).remove();
-         }
+            $('.' + space + ' .player-' + player).remove();
+         },
+          pickPlayers: function() {
+            return [
+              { color: 'red' },
+              { color: 'green' }
+            ];
+          }
       });
+      engine.start();
    });
 });
