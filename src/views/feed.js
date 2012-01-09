18c57d6f19e32131e8efd0684d7cc45e9233b8ee
Michael Dellanoce <michael.dellanoce@gmail.com>
2012-01-09 07:23:15 -0500

    Stub out UI for feeding with resources

--- src/views/feed.js
+++ src/views/feed.js
@@ -14,7 +14,12 @@ define([
       var template = [
         '<p>Player ' + this.model.id + ' you have ' + this.model.get('deficit') + ' starving workers. How will you feed them?</p>',
         '<div>' +
-          '<label><input type="radio" name="feed" class="score" checked="checked" /> Lose 10 points</label>',
+          '<div><label><input type="radio" name="feed" class="score" checked="checked" /> Lose 10 points</label></div>',
+          '<div><label><input type="radio" name="feed" class="resources" /> Lose resources</label></div>',
+          '<div>Wood: <button class="wood">-</button><span class="wood">0</span><button class="wood">+</button></div>' +
+          '<div>Brick: <button class="brick">-</button><span class="brick">0</span><button class="brick">+</button></div>' +
+          '<div>Stone: <button class="stone">-</button><span class="stone">0</span><button class="stone">+</button></div>' +
+          '<div>Gold: <button class="gold">-</button><span class="gold">0</span><button class="gold">+</button></div>' +
         '</div>' +
         '<button class="ok">Ok</button>'
       ];
