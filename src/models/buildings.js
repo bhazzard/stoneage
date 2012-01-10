56cd3e0513545746b9977a53360e56eaddcec84a
Michael Dellanoce <michael.dellanoce@gmail.com>
2012-01-10 07:26:29 -0500

    Add the rest of the fixed cost buildings

--- src/models/buildings.js
+++ src/models/buildings.js
@@ -2,50 +2,147 @@ define([
     'backbone',
     'src/models/building'
   ], function(Backbone, Building) {
-  var buildings = [
-    new Building({  //Building 1
-      value: 13,
-      cost: {
-        wood: 1,
-        brick: 1,
-        gold: 1
-      }
-    }),
-    new Building({  //Building 12
-      value: 14,
-      cost: {
-        wood: 1,
-        stone: 1,
-        gold: 1
-      }
-    }),
-    new Building({  //Building 13
-      value: 10,
-      cost: {
-        wood: 2,
-        brick: 1
-      }
-    }),
-    new Building({  //Building 14
-      value: 13,
-      cost: {
-        wood: 1,
-        brick: 1,
-        gold: 1
-      }
-    })
-  ];
-
   var Buildings = Backbone.Collection.extend({
-    model: Building,
+    model: Building
+  });
+
+  //Special "Deck" class that preloads all of the buildings
+  return Buildings.extend({
+    initialize: function() {
+      var deck = [
+        new Building({  //Building 1
+          value: 13,
+          cost: {
+            wood: 1,
+            brick: 1,
+            gold: 1
+          }
+        }),
+        new Building({  //Building 12
+          value: 14,
+          cost: {
+            wood: 1,
+            stone: 1,
+            gold: 1
+          }
+        }),
+        new Building({  //Building 13
+          value: 10,
+          cost: {
+            wood: 2,
+            brick: 1
+          }
+        }),
+        new Building({  //Building 14
+          value: 13,
+          cost: {
+            wood: 1,
+            brick: 1,
+            gold: 1
+          }
+        }),
+        new Building({  //Building 15
+          value: 13,
+          cost: {
+            brick: 2,
+            stone: 1
+          }
+        }),
+        new Building({  //Building 16
+          value: 14,
+          cost: {
+            brick: 1,
+            stone: 2
+          }
+        }),
+        new Building({  //Building 18
+          value: 11,
+          cost: {
+            wood: 1,
+            brick: 2
+          }
+        }),
+        new Building({  //Building 20
+          value: 12,
+          cost: {
+            wood: 1,
+            brick: 1,
+            stone: 1
+          }
+        }),
+        new Building({  //Building 21
+          value: 13,
+          cost: {
+            wood: 1,
+            stone: 2
+          }
+        }),
+        new Building({  //Building 23
+          value: 14,
+          cost: {
+            wood: 1,
+            stone: 1,
+            gold: 1
+          }
+        }),
+        new Building({  //Building 24
+          value: 16,
+          cost: {
+            stone: 2,
+            gold: 1
+          }
+        }),
+        new Building({  //Building 26
+          value: 14,
+          cost: {
+            brick: 2,
+            gold: 1
+          }
+        }),
+        new Building({  //Building 27
+          value: 11,
+          cost: {
+            wood: 2,
+            stone: 1
+          }
+        }),
+        new Building({  //Building 28
+          value: 12,
+          cost: {
+            wood: 1,
+            brick: 1,
+            stone: 1
+          }
+        }),
+        new Building({  //Building 4
+          value: 15,
+          cost: {
+            brick: 1,
+            stone: 1,
+            gold: 1
+          }
+        }),
+        new Building({  //Building 6
+          value: 12,
+          cost: {
+            wood: 2,
+            gold: 1,
+          }
+        }),
+        new Building({  //Building 8
+          value: 15,
+          cost: {
+            brick: 1,
+            stone: 1,
+            gold: 1
+          }
+        })
+      ];
+
+      this.add(deck);
+    },
     split: function() {
       //TODO - split into 4 buildings collections and return
     }
   });
-
-  //Treat the deck as a sort of singleton by returning
-  //an instance to requirejs
-  var deck = new Buildings();
-  deck.add(buildings);
-  return deck;
 });
