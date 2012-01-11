2428a186b9bf03dc34e751727487f89e2417b194
Michael Dellanoce <michael.dellanoce@gmail.com>
2012-01-11 17:27:10 -0500

    Add the require configuration

--- test/main.js
+++ test/main.js
@@ -1,3 +1,14 @@
+require.config({
+   paths: {
+     'backbone': 'lib/backbone-0.5.3.optamd3',
+     'underscore': 'lib/underscore-1.2.3',
+     'jquery': 'lib/jquery-1.7.1'
+   },
+   baseUrl: ".",
+   waitSeconds: 15,
+   urlArgs: "bust=" +  (new Date()).getTime()
+});
+
 require([], function() {
   test('pass', function() {
     equal(1, 1);
