15e1e854b99d6b96e0ba2f29b14db894eac68c13
Brian Hazzard <bhazzard@gmail.com>
2012-01-15 00:12:57 -0500

    ugly, buggy start for applying tools. Probably need a sub-view to make this better

--- src/views/resolution.js
+++ src/views/resolution.js
@@ -14,13 +14,21 @@ define([
       _(this.options.dice.dice).each(function(die) {
         $('<div />').addClass('die face' + die).appendTo(roll);
       }, this);
+
+      this.options.player.get('tools').each(function(tool) {
+        if (!tool.get('tapped')) {
+          var toolOption = $('<input />').attr('value', tool.get('value')).attr('type', 'checkbox');
+          $(this.el).append(toolOption).append(tool.get('value'));
+        }
+      }, this);
+
       $('<button class="ok">Ok</button>').appendTo(this.el);
       return this;
     },
     resolve: function() {
       this.model.resolve(
         this.options.player,
-        this.options.dice.sum);
+        this.options.dice.sum + _($(this.el).find('input:checked')).reduce(function(memo, el) { return memo + Number($(el).attr('value')); }, 0));
       this.remove();
     }
   });
