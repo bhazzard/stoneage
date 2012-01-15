ffa81fa8968219268e9e4086551ff3db12bd5071
Michael Dellanoce <michael.dellanoce@gmail.com>
2012-01-15 09:44:28 -0500

    Rough visual display of tools on player board

--- src/views/player.js
+++ src/views/player.js
@@ -13,7 +13,14 @@ define([
       $(this.el).empty().addClass('player' + this.model.id);
       $('<div/>').addClass('counter worker-pile').html(this.model.get('workers')).appendTo(this.el);
       $('<div/>').addClass('counter score').html(this.model.get('score')).appendTo(this.el);
-      $('<div/>').addClass('counter tools').html(this.model.get('tools').pluck('value').join('/')).appendTo(this.el);
+      var tools = $('<div/>').addClass('counter tools').appendTo(this.el);
+      this.model.get('tools').each(function(tool) {
+        var toolView = $('<div/>').addClass('tool tool' + tool.get('value'));
+        if (tool.get('tapped')) {
+          toolView.addClass('tapped');
+        }
+        toolView.appendTo(tools);
+      });
       $('<div/>').addClass('counter resource-pile food').html(foodLabel).appendTo(this.el);
       $('<div/>').addClass('counter resource-pile wood').html(this.model.get('wood')).appendTo(this.el);
       $('<div/>').addClass('counter resource-pile brick').html(this.model.get('brick')).appendTo(this.el);
