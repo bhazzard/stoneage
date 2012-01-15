00a9d373a360140325d22f43040ebf25f6bc0499
Michael Dellanoce <michael.dellanoce@gmail.com>
2012-01-15 17:51:13 -0500

    Fix whitespace

--- src/models/tools.js
+++ src/models/tools.js
@@ -5,22 +5,22 @@ define([
   ], function(_, Backbone, Tool) {
   return Backbone.Collection.extend({
     model: Tool,
-	initialize: function() {
+    initialize: function() {
       var that = this;
       _(3).times(function() {
         that.add(new Tool());
       });
-	},
+    },
     upgrade: function() {
       var lowestTool = this.min(function(tool) {
         return tool.get('value');
       });
       lowestTool.upgrade();
     },
-	untapAll: function() {
-	  this.each(function(tool) {
-		tool.untap();
-	  });
-	}
+    untapAll: function() {
+      this.each(function(tool) {
+        tool.untap();
+      });
+    }
   });
 });
